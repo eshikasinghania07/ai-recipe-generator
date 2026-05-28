026ea9f47756b33e68ff05ca7f8fb6e3ba1511ab
Eshika Singhania <eshikasinghania4@gmail.com>
2026-05-28 23:07:04 +0530

    Update tracking.js

--- tracking.js
+++ tracking.js
@@ -1,20 +1,22 @@
 let statusText = document.getElementById("status");
 let progress = document.getElementById("progress");
 
+progress.style.width = "10%";
+
 setTimeout(() => {
     statusText.innerHTML = "🍳 Preparing your food...";
-    progress.style.width = "30%";
+    progress.style.width = "40%";
 }, 2000);
 
 setTimeout(() => {
     statusText.innerHTML = "🛵 Out for Delivery...";
-    progress.style.width = "70%";
+    progress.style.width = "75%";
 }, 5000);
 
 setTimeout(() => {
     statusText.innerHTML = "🎉 Delivered Successfully!";
     progress.style.width = "100%";
-}, 8000);
+}, 9000);
 
 function goHome(){
     window.location.href = "recipes.html";
